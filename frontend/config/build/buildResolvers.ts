183180ebc4f13a337966d466d303ff27296b37c1
zemavex <188645867+zemavex@users.noreply.github.com>
2024-12-27 10:52:31 +0500

    build(front): configure absolute imports

--- frontend/config/build/buildResolvers.ts
+++ frontend/config/build/buildResolvers.ts
@@ -4,5 +4,6 @@ import { BuildOptions } from "./types/config";
 export function buildResolvers(options: BuildOptions): ResolveOptions {
   return {
     extensions: [".tsx", ".ts", ".js"],
+    modules: ["src", "node_modules"],
   };
 }
