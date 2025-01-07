730aa0088d27ba88b6c423759a998c1c042d1dee
zemavex <188645867+zemavex@users.noreply.github.com>
2025-01-07 10:17:32 +0500

    ref(back): change error key from 'error' to 'message'

--- backend/src/rest/middlewares/errorMiddleware.ts
+++ backend/src/rest/middlewares/errorMiddleware.ts
@@ -12,14 +12,14 @@ export const errorMiddleware: ErrorRequestHandler = async (
   console.error(err.stack);
 
   if (err instanceof ZodError) {
-    res.status(400).json({ error: "Validation failed", details: err.errors });
+    res.status(400).json({ message: "Validation failed", details: err.errors });
     return;
   }
 
   if (err instanceof CustomError) {
-    res.status(err.statusCode).json({ error: err.message });
+    res.status(err.statusCode).json({ message: err.message });
     return;
   }
 
-  res.status(500).json({ error: "Unknown error" });
+  res.status(500).json({ message: "Unknown error" });
 };
