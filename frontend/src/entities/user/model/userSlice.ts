b8bcfa1623d55e2283b9bc8479d62600ef19b7fe
zemavex <188645867+zemavex@users.noreply.github.com>
2025-02-04 16:08:54 +0500

    feat(front): send authentication request only when is_authenticated = true in localStorage, change that value in authenticateUser and unauthenticateUser actions

--- frontend/src/entities/user/model/userSlice.ts
+++ frontend/src/entities/user/model/userSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { storage, STORAGE_KEYS } from "shared/lib/storage";
 import type { User } from "./types";
 
 type UserState = {
@@ -24,11 +25,13 @@ export const userSlice = createSlice({
     authenticateUser(state, action: PayloadAction<User>) {
       state.user = action.payload;
       state.authState = "authenticated";
+      storage.set(STORAGE_KEYS.IS_AUTHENTICATED, true);
     },
     setAuthState(state, action: PayloadAction<UserState["authState"]>) {
       state.authState = action.payload;
     },
     unauthenticateUser(state) {
+      storage.remove(STORAGE_KEYS.IS_AUTHENTICATED);
       state.authState = "unauthenticated";
       state.user = initialState.user;
     },
--- frontend/src/features/auth/model/authThunks.ts
+++ frontend/src/features/auth/model/authThunks.ts
@@ -1,14 +1,28 @@
-import { authenticateUser, setAuthState } from "entities/user";
+import { isAxiosError } from "axios";
+import {
+  authenticateUser,
+  setAuthState,
+  unauthenticateUser,
+} from "entities/user";
+import { storage, STORAGE_KEYS } from "shared/lib/storage";
 import { apiAuthenticate } from "../api";
 
 export const authenticateThunk = () => async (dispatch: AppDispatch) => {
+  if (!storage.get(STORAGE_KEYS.IS_AUTHENTICATED)) {
+    dispatch(setAuthState("unauthenticated"));
+    return;
+  }
+
   dispatch(setAuthState("pending"));
 
   try {
     const userData = await apiAuthenticate();
     dispatch(authenticateUser(userData));
   } catch (err) {
-    console.error(err);
+    if (isAxiosError(err) && err.response?.status === 401) {
+      dispatch(unauthenticateUser());
+      return;
+    }
     dispatch(setAuthState("unauthenticated"));
   }
 };
