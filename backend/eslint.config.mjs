bc9a62dd649dfa07bb05063324ee61eda23a25ff
zemavex <188645867+zemavex@users.noreply.github.com>
2025-02-07 17:53:56 +0500

    build(back): remove eslint from ignore, fix package.json scripts, remove include from tsconfig

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -5,14 +5,16 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { ignores: ["dist/", "eslint.config.mjs"] },
+  { ignores: ["dist"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"],
+        },
         tsconfigRootDir: import.meta.dirname,
       },
     },
