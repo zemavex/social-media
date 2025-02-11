4d5c53c960f5f373f3115029e48b44e602040ac9
zemavex <188645867+zemavex@users.noreply.github.com>
2025-02-11 12:29:30 +0500

    build: add eslint to shared workspace, add lint scripts, update lib in all tsconfigs

--- packages/shared/eslint.config.mjs
+++ packages/shared/eslint.config.mjs
@@ -1,4 +1,3 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
@@ -6,7 +5,6 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { ignores: ["dist"] },
-  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
